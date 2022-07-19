a29110e90dc726154db056278ca95fe1f72a2b91
prajesh <jadu.com>
2022-07-19 17:18:36 +0530

    added question 2

---
+++ Q2.2_Return_kth_to_last.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def show(self):
+        node = self.head
+        while node is not None:
+            print(node.data, end=" ")
+            node = node.next
+
+    def add_at_last(self, new_val):
+        node = Node(new_val)
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = node
+
+    # function to remove duplicates from the linked list.
+
+    def getNthFromLast(self,n):
+        lst = []
+        curr = self.head
+
+        while curr is not None:
+            lst.append(curr.data)
+            curr = curr.next
+        
+        if len(lst) < n:
+            return -1
+        else:
+            return lst[-n]
+
+
+l1 = Linkedlist()
+l1.head = Node(6)
+l1.add_at_last(2)
+l1.add_at_last(5)
+l1.add_at_last(9)
+l1.add_at_last(2)
+l1.add_at_last(5)
+l1.show()
+print("")
+c = l1.getNthFromLast(3)
+print(c)
+
